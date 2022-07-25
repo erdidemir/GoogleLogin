9cae0188717e4c076f797172343fdec777688593
ErdiDemir <erdi.demir@icloud.com>
2022-07-25 11:44:21 +0300

    Add project files.

---
+++ GoogleLogin/Contexts/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using GoogleLogin.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleLogin.Contexts
+{
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=GoogleLoginDB;Trusted_Connection=True");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ GoogleLogin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Google.Apis.Auth;
+using GoogleLogin.Contexts;
+using GoogleLogin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly MyContext _context;
+
+        public LoginController()
+        {
+            _context = new MyContext();
+        }
+
+        [HttpPost]
+        public IActionResult GoogleLogin(GoogleLoginModel googleLoginModel)
+        {
+            var payload = GoogleJsonWebSignature.ValidateAsync(googleLoginModel.TokenId, new GoogleJsonWebSignature.ValidationSettings()).Result;
+
+            if (payload is null)
+                return BadRequest("Kullanıcı bulunamadı!");
+
+            var userInDB = _context.Users.FirstOrDefault(x => x.Email == payload.Email);
+
+            if (userInDB is null)
+            {
+                _context.Users.Add(new User { Email = payload.Email, FirstName = payload.GivenName, LastName = payload.FamilyName });
+                _context.SaveChanges();
+            }
+
+            //Login işlemleri
+
+            return Ok();
+
+
+        }
+    }
+}
